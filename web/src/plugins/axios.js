7c3332071a9de3e07d8caa287b79b686755999fb
booscaaa <boscardinvinicius@gmail.com>
2026-04-14 15:25:52 -0300

    🔧 chore(axios): add 401 redirect interceptor and enable withCredentials

--- web/src/plugins/axios.js
+++ web/src/plugins/axios.js
@@ -3,6 +3,18 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: '/api',
   timeout: 15000,
+  withCredentials: true,
 })
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401 && window.location.pathname !== '/login') {
+      localStorage.removeItem('telm-auth')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default api
